0b93028c111d84efe6473dccf45cdbef562727b1
panqiheng <2579671619@qq.com>
2025-10-14 21:12:20 +0800

    10.14

---
+++ src/main/java/top/pan/web/servlet/LoginServlet.java
@@ -0,0 +1,34 @@
+package top.pan.web.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.java.Log;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+
+@Log
+@WebServlet(value = "/login", loadOnStartup = 1)
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().write("我是响应");
+    }
+}
+
